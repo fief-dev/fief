60d0c55dda67c04ca7afa92d8cebf72477198d21
François Voron <fvoron@gmail.com>
2022-03-03 11:18:31 +0100

    Improve frontend static files serving

--- backend/fief/apps/admin_frontend/app.py
+++ backend/fief/apps/admin_frontend/app.py
@@ -1,40 +1,36 @@
-import os
-import stat
+from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException
 
 from fief.paths import STATIC_DIRECTORY
+
+
+class StaticFilesFallback(StaticFiles):
+    def __init__(self, *args, fallback: Path, **kwargs) -> None:
+        self.fallback = fallback
+        super().__init__(*args, **kwargs)
+
+    async def get_response(self, *args, **kwargs) -> Response:
+        try:
+            return await super().get_response(*args, **kwargs)
+        except HTTPException as e:
+            if e.status_code == status.HTTP_404_NOT_FOUND:
+                return FileResponse(self.fallback)
+            raise
+
 
 app = FastAPI()
 
 app.mount(
-    "/static",
-    StaticFiles(
-        directory=STATIC_DIRECTORY / "frontend" / "static", html=True, check_dir=False
+    "/",
+    StaticFilesFallback(
+        directory=STATIC_DIRECTORY / "frontend",
+        fallback=STATIC_DIRECTORY / "frontend" / "index.html",
+        html=False,
+        check_dir=False,
     ),
-    name="admin_frontend:static",
+    name="admin_frontend:frontend",
 )
-
-app.mount(
-    "/locales",
-    StaticFiles(
-        directory=STATIC_DIRECTORY / "frontend" / "locales", html=True, check_dir=False
-    ),
-    name="admin_frontend:locales",
-)
-
-
-@app.get("/{path:path}", name="admin_frontend:index")
-async def main(path: str):
-    full_path = STATIC_DIRECTORY / "frontend" / path
-
-    try:
-        path_stat = os.stat(full_path)
-        if stat.S_ISREG(path_stat.st_mode):
-            return FileResponse(full_path)
-    except FileNotFoundError:
-        pass
-
-    return FileResponse(STATIC_DIRECTORY / "frontend" / "index.html")
