15ed5c451da347db93ae9d7786f57da0d63782f5
François Voron <fvoron@gmail.com>
2022-04-17 17:43:49 +0200

    Improve dashboard with a welcome message and useful links

--- admin-frontend/src/routes/Dashboard/Dashboard.tsx
+++ admin-frontend/src/routes/Dashboard/Dashboard.tsx
@@ -1,11 +1,33 @@
+import { useTranslation } from 'react-i18next';
 import Layout from '../../components/Layout/Layout';
-import { useCurrentUser } from '../../hooks/user';
 
 const Dashboard: React.FunctionComponent = () => {
-  const user = useCurrentUser();
+  const { t } = useTranslation(['dashboard']);
+
   return (
     <Layout>
-      Hello {user.email}
+      <div className="relative bg-slate-200 p-4 sm:p-6 rounded-sm overflow-hidden mb-8 ">
+        <div className="relative">
+          <h1 className="text-2xl md:text-3xl text-slate-800 font-bold mb-1">{t('greet')}</h1>
+          <p className="mb-4">{t('message')}</p>
+          <a
+            className="btn bg-primary-500 hover:bg-primary-600 text-white"
+            href="https://docs.fief.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t('documentation')}
+          </a>
+          <a
+            className="btn bg-white border-slate-200 hover:border-slate-300 text-slate-600 ml-2"
+            href="https://github.com/fief-dev/fief/discussions"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t('ask')}
+          </a>
+        </div>
+      </div>
     </Layout>
   );
 };
