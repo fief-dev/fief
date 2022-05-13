3d3dd8f5ea59d41b0157b26a0e216f32e4bee93b
François Voron <fvoron@gmail.com>
2022-05-13 12:03:29 +0200

    Improve form data parsing

--- backend/fief/dependencies/form.py
+++ backend/fief/dependencies/form.py
@@ -1,22 +1,18 @@
-from collections import defaultdict
 from typing import Any, Dict
 
 from fastapi import Request
 
 
-def recursive_defaultdict_factory():
-    return defaultdict(recursive_defaultdict_factory)
-
-
 async def get_form_data_dict(request: Request) -> Dict[str, Any]:
     form = await request.form()
-    form_data_dict: Dict[str, Any] = defaultdict(recursive_defaultdict_factory)
+    form_data_dict: Dict[str, Any] = {}
 
     for field in form:
         data_dict = form_data_dict
         path = field.split(".")
         for key in path[:-1]:
-            data_dict = data_dict[key]
-        data_dict[path[-1]] = form[field]
+            data_dict = data_dict.setdefault(key, {})
+        if form[field] != "":
+            data_dict[path[-1]] = form[field]
 
     return form_data_dict
