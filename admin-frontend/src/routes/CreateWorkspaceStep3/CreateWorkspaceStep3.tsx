0cd9fd5ced5089cd2822bf2a51ef9fa5187ad5b6
François Voron <fvoron@gmail.com>
2022-06-17 09:23:29 +0200

    Add field to set SSL mode in Create Workspace wizard

--- admin-frontend/src/routes/CreateWorkspaceStep3/CreateWorkspaceStep3.tsx
+++ admin-frontend/src/routes/CreateWorkspaceStep3/CreateWorkspaceStep3.tsx
@@ -1,10 +1,11 @@
-import { useCallback, useContext, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { Link, useNavigate } from 'react-router-dom';
 
 import ErrorAlert from '../../components/ErrorAlert/ErrorAlert';
 import FormErrorMessage from '../../components/FormErrorMessage/FormErrorMessage';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 import OnboardingLayout from '../../components/OnboardingLayout/OnboardingLayout';
 import SuccessAlert from '../../components/SuccessAlert/SuccessAlert';
 import CreateWorkspaceContext from '../../contexts/create-workspace';
@@ -17,15 +18,17 @@ const CreateWorkspaceStep3: React.FunctionComponent<React.PropsWithChildren<unkn
   const api = useAPI();
   const navigate = useNavigate();
 
+  const [checkConnectionLoading, setCheckConnectionLoading] = useState(false);
   const [checkConnectionSuccess, setCheckConnectionSuccess] = useState(false);
   const [checkConnectionError, setCheckConnectionError] = useState<string | undefined>(undefined);
 
   const [createWorkspace, setCreateWorkspace] = useContext(CreateWorkspaceContext);
-  const { register, handleSubmit, setError, formState: { errors } } = useForm<schemas.workspace.WorkspaceCreate>({ defaultValues: createWorkspace });
+  const { register, handleSubmit, setError, watch, resetField, formState: { errors } } = useForm<schemas.workspace.WorkspaceCreate>({ defaultValues: createWorkspace });
   const fieldRequiredErrorMessage = useFieldRequiredErrorMessage();
   const handleAPIError = useAPIErrorHandler(setCheckConnectionError, setError);
 
   const onCheckConnection: SubmitHandler<schemas.workspace.WorkspaceCreate> = useCallback(async (data) => {
+    setCheckConnectionLoading(true);
     setCheckConnectionSuccess(false);
     setCheckConnectionError(undefined);
     try {
@@ -34,6 +37,7 @@ const CreateWorkspaceStep3: React.FunctionComponent<React.PropsWithChildren<unkn
     } catch (err) {
       handleAPIError(err);
     }
+    setCheckConnectionLoading(false);
   }, [api, handleAPIError]);
 
   const onSubmit: SubmitHandler<schemas.workspace.WorkspaceCreate> = useCallback((data) => {
@@ -43,6 +47,23 @@ const CreateWorkspaceStep3: React.FunctionComponent<React.PropsWithChildren<unkn
     });
     navigate('/create-workspace/step4');
   }, [createWorkspace, setCreateWorkspace, navigate]);
+
+  const [sslModes, setSSLModes] = useState<string[]>([]);
+  const databaseType = watch('database_type');
+  useEffect(() => {
+    if (databaseType) {
+      const sslModes = Object.values(schemas.workspace.SSL_MODES[databaseType]);
+      setSSLModes(sslModes);
+    }
+  }, [databaseType]);
+
+  useEffect(() => {
+    if (sslModes.length > 0) {
+      resetField('database_ssl_mode');
+    }
+  }, [sslModes, resetField]);
+
+  const databaseSSLMode = watch('database_ssl_mode');
 
   return (
     <OnboardingLayout active={3}>
@@ -116,14 +137,33 @@ const CreateWorkspaceStep3: React.FunctionComponent<React.PropsWithChildren<unkn
             />
             <FormErrorMessage errors={errors} name="database_name" />
           </div>
+          <div>
+            <label className="block text-sm font-medium mb-1" htmlFor="database_ssl_mode">{t('create.database_ssl_mode')}</label>
+            <select
+              id="database_ssl_mode"
+              className="form-select w-full"
+              {...register('database_ssl_mode', { required: fieldRequiredErrorMessage })}
+            >
+              {sslModes.map((type) =>
+                <option key={type} value={type}>{t(`database_ssl_mode.${type}`)}</option>
+              )}
+            </select>
+            <FormErrorMessage errors={errors} name="database_ssl_mode" />
+            {databaseSSLMode && !schemas.workspace.isSafeSSLMode(databaseSSLMode) &&  <div className="text-xs mt-1 text-yellow-500">{t('create.unsafe_ssl_mode')}</div>}
+          </div>
           {checkConnectionError && <ErrorAlert message={t('create.check_connection_error', { error: checkConnectionError })} />}
           {checkConnectionSuccess && <SuccessAlert message={t('create.check_connection_success')} />}
         </div>
         <div className="flex items-center justify-between">
           <Link className="text-sm underline hover:no-underline" to="../step2">&lt;- {t('create.back')}</Link>
           <div>
-            <button type="button" className="text-sm underline hover:no-underline mr-2" onClick={handleSubmit(onCheckConnection)}>{t('create.check_connection')}</button>
-            <button type="submit" className="btn bg-primary-500 hover:bg-primary-600 text-white ml-auto">{t('create.next')} -&gt;</button>
+            {checkConnectionLoading && <LoadingSpinner className="w-4 h-4 text-primary" />}
+            {!checkConnectionLoading &&
+              <>
+                <button type="button" className="text-sm underline hover:no-underline mr-2" onClick={handleSubmit(onCheckConnection)}>{t('create.check_connection')}</button>
+                <button type="submit" className="btn bg-primary-500 hover:bg-primary-600 text-white ml-auto" disabled={checkConnectionLoading}>{t('create.next')} -&gt;</button>
+              </>
+            }
           </div>
         </div>
       </form>
--- admin-frontend/src/schemas/workspace.ts
+++ admin-frontend/src/schemas/workspace.ts
@@ -5,6 +5,40 @@ export enum DatabaseType {
   MYSQL = "MYSQL",
 }
 
+export enum PostreSQLSSLMode {
+  DISABLE = "disable",
+  ALLOW = "allow",
+  PREFER = "prefer",
+  REQUIRE = "require",
+  VERIFY_CA = "verify-ca",
+  VERIFY_FULL = "verify-full",
+}
+
+export enum MySQLSSLMode {
+  DISABLED = "DISABLED",
+  PREFERRED = "PREFERRED",
+  REQUIRED = "REQUIRED",
+  VERIFY_CA = "VERIFY_CA",
+  VERIFY_IDENTITY = "VERIFY_IDENTITY",
+}
+
+export const SSL_MODES: Record<DatabaseType, typeof PostreSQLSSLMode | typeof MySQLSSLMode> = {
+  [DatabaseType.POSTGRESQL]: PostreSQLSSLMode,
+  [DatabaseType.MYSQL]: MySQLSSLMode,
+}
+
+export const isSafeSSLMode = (mode: string): boolean => {
+  const safeModes = [
+    PostreSQLSSLMode.REQUIRE,
+    PostreSQLSSLMode.VERIFY_CA,
+    PostreSQLSSLMode.VERIFY_FULL,
+    MySQLSSLMode.REQUIRED,
+    MySQLSSLMode.VERIFY_CA,
+    MySQLSSLMode.VERIFY_IDENTITY,
+  ];
+  return safeModes.includes(mode as PostreSQLSSLMode | MySQLSSLMode);
+};
+
 export interface WorkspaceCheckConnection {
   database_type: DatabaseType;
   database_host: string;
@@ -22,6 +56,7 @@ export interface WorkspaceCreate {
   database_username: string;
   database_password: string;
   database_name: string;
+  database_ssl_mode: string;
 }
 
 interface BaseWorkspace extends UUIDSchema {
