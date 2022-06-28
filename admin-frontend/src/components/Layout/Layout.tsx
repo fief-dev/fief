3ad7fa2a6f81574f016ade02451a74f2a6856ba2
François Voron <fvoron@gmail.com>
2022-06-28 16:18:42 +0200

    Fix Layout when main zone is large

--- admin-frontend/src/components/Layout/Layout.tsx
+++ admin-frontend/src/components/Layout/Layout.tsx
@@ -18,13 +18,13 @@ const Layout: React.FunctionComponent<React.PropsWithChildren<LayoutProps>> = ({
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <main>
-          <div className="lg:relative lg:flex">
-            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          <div className="grid grid-cols-12">
+            <div className={`px-4 sm:px-6 lg:px-8 py-8 col-span-12 ${sidebar ? 'lg:col-span-9' : ''}`}>
               {children}
             </div>
             {sidebar &&
-              <div>
-                <div className="lg:sticky lg:top-16 bg-slate-50 lg:overflow-x-hidden lg:overflow-y-auto no-scrollbar lg:shrink-0 border-t lg:border-t-0 lg:border-l border-slate-200 lg:w-[390px] lg:h-[calc(100vh-64px)]">
+              <div className="col-span-12 lg:col-span-3">
+                <div className="lg:sticky lg:top-16 bg-slate-50 lg:overflow-x-hidden lg:overflow-y-auto no-scrollbar lg:shrink-0 border-t lg:border-t-0 lg:border-l border-slate-200">
                   <div className="py-8 px-4 lg:px-8">
                     <div className="max-w-sm mx-auto lg:max-w-none">
                       {sidebar}
