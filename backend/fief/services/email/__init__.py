c923332d818b6ebfcc3fb4ba7d3bd01a8ae34628
Rui Oliveira <ruipedrooliveira@ua.pt>
2022-10-27 12:35:53 +0100

    Added sendgrid integration (#104)
    * Added sendgrid integration
    
    * Improvements on sendgrid service. Fixed sintaxe.
    
    * Fix import
    
    * Fix import
    
    Co-authored-by: Rui Oliveira <rui.oliveira@wegoshare.com>

--- backend/fief/services/email/__init__.py
+++ backend/fief/services/email/__init__.py
@@ -4,6 +4,7 @@ from typing import Dict, Type
 from fief.services.email.base import EmailError, EmailProvider, SendEmailError
 from fief.services.email.null import Null
 from fief.services.email.postmark import Postmark
+from fief.services.email.sendgrid import Sendgrid
 from fief.services.email.smtp import SMTP
 
 
@@ -11,12 +12,14 @@ class AvailableEmailProvider(str, Enum):
     NULL = "NULL"
     POSTMARK = "POSTMARK"
     SMTP = "SMTP"
+    SENDGRID = "SENDGRID"
 
 
 EMAIL_PROVIDERS: Dict[AvailableEmailProvider, Type[EmailProvider]] = {
     AvailableEmailProvider.NULL: Null,
     AvailableEmailProvider.POSTMARK: Postmark,
     AvailableEmailProvider.SMTP: SMTP,
+    AvailableEmailProvider.SENDGRID: Sendgrid,
 }
 
 __all__ = [
