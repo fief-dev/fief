21e132ce20fed867893d4ad7751f24f8ec5177fe
Rui Oliveira <rui.oliveira@wegoshare.com>
2022-11-06 21:07:04 +0000

    change like to ilike

--- backend/fief/repositories/workspace.py
+++ backend/fief/repositories/workspace.py
@@ -14,8 +14,7 @@ class WorkspaceRepository(BaseRepository[Workspace], UUIDRepositoryMixin[Workspa
     model = Workspace
 
     async def get_by_domain(self, domain: str) -> Optional[Workspace]:
-      
-        statement = select(Workspace).filter(Workspace.domain.like("%{}%".format(domain)))
+        statement = select(Workspace).filter(Workspace.domain.ilike(f"%{domain}%"))
         return await self.get_one_or_none(statement)
 
     async def get_main(self) -> Optional[Workspace]:
