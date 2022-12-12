9b7c6bc12ad46a943e5933a171521184c662108c
François Voron <fvoron@gmail.com>
2022-12-12 08:59:00 +0100

    Remove old admin_frontend app

--- backend/fief/apps/__init__.py
+++ backend/fief/apps/__init__.py
@@ -1,6 +1,5 @@
 from fief.apps.admin.app import app as admin_app
-from fief.apps.admin_frontend.app import app as admin_frontend_app
 from fief.apps.admin_dashboard.app import app as admin_dashboard_app
 from fief.apps.auth.app import app as auth_app
 
-__all__ = ["admin_app", "admin_frontend_app", "auth_app", "admin_dashboard_app"]
+__all__ = ["admin_app", "auth_app", "admin_dashboard_app"]
