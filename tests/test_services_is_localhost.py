be667efd1f67ecafbbf6b83f6f4b5cd75d2eb819
François Voron <fvoron@gmail.com>
2023-03-09 14:21:25 +0100

    Add unit test for is_localhost function

---
+++ tests/test_services_is_localhost.py
@@ -0,0 +1,15 @@
+import pytest
+
+from fief.services.localhost import is_localhost
+
+
+@pytest.mark.parametrize("host,result", [
+    ("127.0.0.1", True),
+    ("localhost", True),
+    ("192.168.0.42", True),
+    ("192.168.1.1", True),
+    ("www.bretagne.duchy", False),
+    ("www.fief.dev", False),
+])
+def test_is_localhost(host: str, result: bool):
+    assert is_localhost(host) == result
