8db867ed91af83a67c2a30ba345baf8b5bac9ecc
François Voron <fvoron@gmail.com>
2023-04-18 08:08:14 +0000

    Fix post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,8 @@
 set -e
 
 hatch env create
+npm install
+
 cp .env.dist .env
 echo "ENCRYPTION_KEY=$(hatch run python -c "from cryptography.fernet import Fernet;print(Fernet.generate_key().decode('utf-8'), end='')")" >> .env
 echo "FIEF_DOMAIN=${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" >> .env
@@ -13,4 +15,4 @@ hatch run python -m fief.cli workspaces create-main
 hatch run python -m fief.cli workspaces create-main-user --user-email anne@bretagne.duchy --user-password hermine1
 set -e
 
-export FORWARDED_ALLOW_IPS=*
+export FORWARDED_ALLOW_IPS='*'
