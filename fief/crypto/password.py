9c183fffc3fca722d6832a7ea4492df475451319
François Voron <fvoron@gmail.com>
2023-05-12 09:05:42 +0200

    Increase password generator entropy to make sure it passes strength validation

--- fief/crypto/password.py
+++ fief/crypto/password.py
@@ -1,5 +1,13 @@
-from fastapi_users.password import PasswordHelper
+from fastapi_users.password import PasswordHelper as BasePasswordHelper
 from passlib.context import CryptContext
+from passlib import pwd
 
 context = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
+
+
+class PasswordHelper(BasePasswordHelper):
+    def generate(self) -> str:
+        return pwd.genword(entropy=128)
+
+
 password_helper = PasswordHelper(context)
