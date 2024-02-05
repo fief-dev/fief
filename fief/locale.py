87a83e9d5e00232d16e4e2ad0a1a9beb81455405
François Voron <fvoron@gmail.com>
2024-02-05 14:57:01 +0100

    Revamp password validation so it doesn't depend on Settings

--- fief/locale.py
+++ fief/locale.py
@@ -1,37 +1,6 @@
-from collections.abc import Mapping
-from typing import Any
-
-import asgi_babel
-import wtforms.i18n
-from asgi_tools import Request
-from babel import Locale, UnknownLocaleError, support
-
-from fief.paths import LOCALE_DIRECTORY
-from fief.settings import settings
+from babel import Locale, support
 
 Translations = support.Translations
-
-
-async def select_locale(request: Request) -> str | None:
-    user_locale_cookie = request.cookies.get(settings.user_locale_cookie_name)
-    if user_locale_cookie is not None:
-        try:
-            Locale.parse(user_locale_cookie, sep="-")
-        except (ValueError, UnknownLocaleError):
-            pass
-        else:
-            return user_locale_cookie
-    return await asgi_babel.select_locale_by_request(request)
-
-
-class BabelMiddleware(asgi_babel.BabelMiddleware):
-    def __post_init__(self):
-        self.locale_selector = select_locale
-        return super().__post_init__()
-
-
-def get_babel_middleware_kwargs() -> Mapping[str, Any]:
-    return dict(locales_dirs=[LOCALE_DIRECTORY, wtforms.i18n.messages_path()])
 
 
 def get_translations(
