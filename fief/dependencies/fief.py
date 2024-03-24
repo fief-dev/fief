d1f3a2121d7b13b4b3ddcdf5c670306846544a5d
François Voron <fvoron@gmail.com>
2024-03-24 15:16:31 +0100

    Detect if Uvicorn runs with SSL config for Fief-ception

--- fief/dependencies/fief.py
+++ fief/dependencies/fief.py
@@ -1,13 +1,44 @@
-from fief_client import FiefAsync
+import asyncio
 
+from fief_client import FiefAsync
+from uvicorn.server import Server
+
+from fief.logger import logger
 from fief.settings import settings
 
+
+def _is_uvicorn_ssl() -> bool:
+    """
+    Hacky way to determine if the Uvicorn server is running with SSL,
+    by retrieving the server instance from the running asyncio tasks.
+    """
+    try:
+        for task in asyncio.all_tasks():
+            coroutine = task.get_coro()
+            frame = coroutine.cr_frame
+            args = frame.f_locals
+            if self := args.get("self"):
+                if isinstance(self, Server):
+                    return self.config.is_ssl
+    except RuntimeError:
+        pass
+    return False
+
+
+_is_ssl = _is_uvicorn_ssl()
+_scheme = "https" if _is_ssl else "http"
+if _is_ssl:
+    logger.debug("Uvicorn server is running with SSL")
+else:
+    logger.debug("Uvicorn server is running without SSL")
+
 fief = FiefAsync(
-    f"http://localhost:{settings.port}",  # Always call Fief on localhost
+    f"{_scheme}://localhost:{settings.port}",  # Always call Fief on localhost
     settings.fief_client_id,
     settings.fief_client_secret,
     encryption_key=settings.fief_encryption_key,
     host=settings.fief_domain,
+    verify=False,
 )
 
 
