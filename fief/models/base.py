d36c82f7ce2b44cec0dfda2803d7d41b0f8385de
François Voron <fvoron@gmail.com>
2024-08-29 09:02:57 +0200

    Add migration for unique constraint on OAuthAccount provider_id and account_id

--- fief/models/base.py
+++ fief/models/base.py
@@ -1,5 +1,6 @@
 import os
 
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 
 from fief.settings import settings
@@ -16,6 +17,16 @@ def get_prefixed_tablename(name: str) -> str:
 
 
 class Base(DeclarativeBase):
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "%(table_name)s_%(column_0_N_name)s_key",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
     def __init_subclass__(cls) -> None:
         cls.__tablename__ = get_prefixed_tablename(cls.__tablename__)
         super().__init_subclass__()
