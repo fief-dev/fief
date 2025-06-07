387cb75e97133cefa3d84b636d1d05853587d585
François Voron <fvoron@gmail.com>
2025-06-07 14:33:09 +0200

    Add async version of AuthManager

--- fief/_auth.py
+++ fief/_auth.py
@@ -3,8 +3,13 @@
 import typing
 
 from fief._exceptions import FiefException
-from fief.methods import AuthenticateKwargs, EnrollKwargs, MethodProtocol
-from fief.storage import StorageProtocol
+from fief.methods import (
+    AsyncMethodProtocol,
+    AuthenticateKwargs,
+    EnrollKwargs,
+    MethodProtocol,
+)
+from fief.storage import AsyncStorageProtocol, StorageProtocol
 
 
 class AuthException(FiefException):
@@ -44,7 +49,22 @@ U = typing.TypeVar("U", bound=UserProtocol)
 """Generic type variable for user models."""
 
 
-class AuthManager(typing.Generic[U]):
+class _AuthManagerBase(typing.Generic[U]):
+    """Base class for auth managers."""
+
+    model: type[U]
+
+    def _validate_identifier_fields(
+        self, fields: dict[str, typing.Any]
+    ) -> tuple[str, ...]:
+        identifier_fields = self.model.identifier_fields()
+        missing_fields = [field for field in identifier_fields if field not in fields]
+        if len(missing_fields) > 0:
+            raise MissingIdentifierFieldsException(missing_fields)
+        return identifier_fields
+
+
+class AuthManager(_AuthManagerBase[U]):
     """Class to manage user authentication and methods."""
 
     def __init__(self, model: type[U], storage: StorageProtocol[U]) -> None:
@@ -122,19 +142,90 @@ class AuthManager(typing.Generic[U]):
 
         return None
 
-    def _validate_identifier_fields(
-        self, fields: dict[str, typing.Any]
-    ) -> tuple[str, ...]:
-        identifier_fields = self.model.identifier_fields()
-        missing_fields = [field for field in identifier_fields if field not in fields]
-        if len(missing_fields) > 0:
-            raise MissingIdentifierFieldsException(missing_fields)
-        return identifier_fields
+
+class AsyncAuthManager(_AuthManagerBase[U]):
+    """Async class to manage user authentication and methods."""
+
+    def __init__(self, model: type[U], storage: AsyncStorageProtocol[U]) -> None:
+        self.model = model
+        self.storage = storage
+
+    async def signup(
+        self,
+        method: AsyncMethodProtocol[EnrollKwargs, AuthenticateKwargs],
+        enroll_data: EnrollKwargs,
+        **fields: typing.Any,
+    ) -> U:
+        """
+        Sign up a new user using the specified authentication method and fields.
+
+        Args:
+            method_cls: The authentication method class to use.
+            enroll_data: The data for the method's enroll function.
+            **fields: The user fields to create the new user. It must contain
+                all the identifier fields.
+
+        Returns:
+            The newly created user.
+
+        Raises:
+            UserAlreadyExistsException: If a user already exists with the given identifier fields.
+            MissingIdentifierFieldsException: If any of the identifier fields are missing.
+            UnknownMethodException: If the authentication method is unknown.
+        """
+        identifier_fields = self._validate_identifier_fields(fields)
+
+        existing_user = await self.storage.get_one(
+            **{field: fields[field] for field in identifier_fields}
+        )
+        if existing_user is not None:
+            raise UserAlreadyExistsException(
+                {field: fields[field] for field in identifier_fields}
+            )
+
+        user = await self.storage.create(**fields)
+        await method.enroll(user.id, enroll_data)
+
+        return user
+
+    async def signin(
+        self,
+        method: AsyncMethodProtocol[EnrollKwargs, AuthenticateKwargs],
+        auth_data: AuthenticateKwargs,
+        **fields: typing.Any,
+    ) -> U | None:
+        """
+        Sign in a user using the specified authentication method and fields.
+
+        Args:
+            method_cls: The authentication method class to use.
+            auth_data: The data for the method's authenticate function.
+            **fields: The fields to identify the user.
+
+        Returns:
+            The authenticated user or None if authentication fails.
+
+        Raises:
+            MissingIdentifierFieldsException: If any of the identifier fields are missing.
+            UnknownMethodException: If the authentication method is unknown.
+        """
+        identifier_fields = self._validate_identifier_fields(fields)
+
+        user = await self.storage.get_one(
+            **{field: fields[field] for field in identifier_fields}
+        )
+        user_id = user.id if user else None
+
+        if await method.authenticate(user_id, auth_data) and user is not None:
+            return user
+
+        return None
 
 
 __all__ = [
     "U",
     "AuthManager",
+    "AsyncAuthManager",
     "AuthException",
     "MissingIdentifierFieldsException",
     "UserAlreadyExistsException",
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,12 +4,17 @@ import uuid
 import pytest
 
 from fief._auth import (
+    AsyncAuthManager,
     AuthManager,
     MissingIdentifierFieldsException,
     UserAlreadyExistsException,
 )
-from fief.methods.password import PasswordMethod, PasswordMethodModelData
-from tests.fixtures import MockStorage, UserModel
+from fief.methods.password import (
+    PasswordAsyncMethod,
+    PasswordMethod,
+    PasswordMethodModelData,
+)
+from tests.fixtures import MockAsyncStorage, MockStorage, UserModel
 
 
 @dataclasses.dataclass
@@ -33,6 +38,23 @@ def storage() -> MockStorage[UserModel]:
 @pytest.fixture
 def auth_manager(storage: MockStorage[UserModel]) -> AuthManager[UserModel]:
     return AuthManager(UserModel, storage)
+
+
+@pytest.fixture
+def password_async_method() -> PasswordAsyncMethod[PasswordMethodModel]:
+    return PasswordAsyncMethod(storage=MockAsyncStorage(PasswordMethodModel))
+
+
+@pytest.fixture
+def async_storage() -> MockAsyncStorage[UserModel]:
+    return MockAsyncStorage(UserModel)
+
+
+@pytest.fixture
+def async_auth_manager(
+    async_storage: MockAsyncStorage[UserModel],
+) -> AsyncAuthManager[UserModel]:
+    return AsyncAuthManager(UserModel, async_storage)
 
 
 class TestSignup:
@@ -127,6 +149,111 @@ class TestSignin:
 
         authenticated_user = auth_manager.signin(
             password_method, {"password": "richard"}, email="alienor@aquitaine.duchy"
+        )
+        assert authenticated_user is not None
+        assert authenticated_user.id == user.id
+
+
+@pytest.mark.anyio
+class TestAsyncSignup:
+    async def test_missing_identifier_fields(
+        self,
+        async_auth_manager: AsyncAuthManager[UserModel],
+        password_async_method: PasswordAsyncMethod[PasswordMethodModel],
+    ) -> None:
+        with pytest.raises(MissingIdentifierFieldsException):
+            await async_auth_manager.signup(
+                password_async_method, {"password": "richard"}, name="Aliénor"
+            )
+
+    async def test_user_already_exists(
+        self,
+        async_auth_manager: AsyncAuthManager[UserModel],
+        async_storage: MockAsyncStorage[UserModel],
+        password_async_method: PasswordAsyncMethod[PasswordMethodModel],
+    ) -> None:
+        await async_storage.create(email="alienor@aquitaine.duchy")
+
+        with pytest.raises(UserAlreadyExistsException):
+            await async_auth_manager.signup(
+                password_async_method,
+                {"password": "richard"},
+                email="alienor@aquitaine.duchy",
+            )
+
+    async def test_valid(
+        self,
+        async_auth_manager: AsyncAuthManager[UserModel],
+        password_async_method: PasswordAsyncMethod[PasswordMethodModel],
+    ) -> None:
+        user = await async_auth_manager.signup(
+            password_async_method,
+            {"password": "richard"},
+            email="alienor@aquitaine.duchy",
+        )
+        assert user.id is not None
+        assert user.email == "alienor@aquitaine.duchy"
+
+        password_method_model = await password_async_method._storage.get_one(
+            user_id=user.id, name=password_async_method.name
+        )
+        assert password_method_model is not None
+        assert password_method_model.user_id == user.id
+
+
+@pytest.mark.anyio
+class TestAsyncSignin:
+    async def test_missing_identifier_fields(
+        self,
+        async_auth_manager: AsyncAuthManager[UserModel],
+        password_async_method: PasswordAsyncMethod[PasswordMethodModel],
+    ) -> None:
+        with pytest.raises(MissingIdentifierFieldsException):
+            await async_auth_manager.signin(
+                password_async_method, {"password": "richard"}, name="Aliénor"
+            )
+
+    async def test_non_existing_user(
+        self,
+        async_auth_manager: AsyncAuthManager[UserModel],
+        password_async_method: PasswordAsyncMethod[PasswordMethodModel],
+    ) -> None:
+        user = await async_auth_manager.signin(
+            password_async_method,
+            {"password": "richard"},
+            email="alienor@aquitaine.duchy",
+        )
+        assert user is None
+
+    async def test_invalid_authentication(
+        self,
+        async_auth_manager: AsyncAuthManager[UserModel],
+        async_storage: MockAsyncStorage[UserModel],
+        password_async_method: PasswordAsyncMethod[PasswordMethodModel],
+    ) -> None:
+        user = await async_storage.create(email="alienor@aquitaine.duchy")
+        await password_async_method.enroll(user.id, {"password": "richard"})
+
+        authenticated_user = await async_auth_manager.signin(
+            password_async_method,
+            {"password": "wrongpassword"},
+            email="alienor@aquitaine.duchy",
+        )
+        assert authenticated_user is None
+
+    async def test_valid_authentication(
+        self,
+        async_auth_manager: AsyncAuthManager[UserModel],
+        async_storage: MockAsyncStorage[UserModel],
+        password_async_method: PasswordAsyncMethod[PasswordMethodModel],
+    ) -> None:
+        user = await async_storage.create(email="alienor@aquitaine.duchy")
+        await password_async_method.enroll(user.id, {"password": "richard"})
+
+        authenticated_user = await async_auth_manager.signin(
+            password_async_method,
+            {"password": "richard"},
+            email="alienor@aquitaine.duchy",
         )
         assert authenticated_user is not None
         assert authenticated_user.id == user.id
